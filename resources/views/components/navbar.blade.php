2169b975634baff31945c896d82a64d05d0c3e9e
Dimasalalu <dimaswahyuuu2005@gmail.com>
2026-09-19 12:29:41 +0700

    memperbaiki navigasi

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -8,13 +8,13 @@
                             </div>
                             <div class="hidden md:block">
                                 <div class="ml-10 flex items-baseline space-x-4">
-                                    <x-nav-link href="/" :current="request()->is('/')">Home</x-nav-link>
+                                    <x-my-nav-link href="/" :current="request()->is('/')">Home</x-my-nav-link>
 
-                                    <x-nav-link href="/posts" :current="request()->is('posts')">Blog</x-nav-link>
+                                    <x-my-nav-link href="/posts" :current="request()->is('posts')">Blog</x-my-nav-link>
 
-                                    <x-nav-link href="/about" :current="request()->is('about')">about</x-nav-link>
+                                    <x-my-nav-link href="/about" :current="request()->is('about')">about</x-my-nav-link>
 
-                                    <x-nav-link href="/contact" :current="request()->is('contact')">contact</x-nav-link>
+                                    <x-my-nav-link href="/contact" :current="request()->is('contact')">contact</x-my-nav-link>
                                 </div>
                             </div>
                         </div>
@@ -34,33 +34,63 @@
 
                                 <!-- Profile dropdown -->
                                 <el-dropdown class="relative ml-3">
-                                    <button
-                                        class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-                                        <span class="absolute -inset-1.5"></span>
-                                        <span class="sr-only">Open user menu</span>
-                                        <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                            alt=""
-                                            class="size-8 rounded-full outline -outline-offset-1 outline-white/10" />
-                                    </button>
+
+                                    {{-- jika user sudah login --}}
+                                    @auth
+                                        <button
+                                            class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 cursor-pointer">
+                                            <span class="absolute -inset-1.5"></span>
+                                            <span class="sr-only">Open user menu</span>
+                                            <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                                alt=""
+                                                class="size-8 rounded-full outline -outline-offset-1 outline-white/10" />
+                                            <div class="text-white text-sm font-medium ml-3">{{ Auth::user()->name }}</div>
+
+                                            <div class="ms-1">
+                                                <svg class="fill-current h-4 w-4 text-white"
+                                                    xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                                    <path fill-rule="evenodd"
+                                                        d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                        clip-rule="evenodd" />
+                                                </svg>
+                                            </div>
+                                        </button>
+                                    @endauth
+
+                                    {{-- jika user belum login --}}
+                                    @guest
+                                        <a href="/login" class="text-white text-sm font-medium">login</a>
+                                        <span class="text-white text-sm">|</span>
+                                        <a href="/login" class="text-white text-sm font-medium">register</a>
+
+                                    @endguest
 
                                     <el-menu anchor="bottom end" popover
                                         class="w-48 origin-top-right rounded-md bg-gray-800 py-1 outline-1 -outline-offset-1 outline-white/10 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in">
-                                        <a href="#"
-                                            class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">Your
-                                            profile</a>
-                                        <a href="#"
-                                            class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">Settings</a>
-                                        <a href="#"
-                                            class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">Sign
-                                            out</a>
+
+                                        {{-- To Dasboard --}}
+                                        <a href="/dashboard"
+                                            class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">Dashboard</a>
+                                        {{-- To Profile  --}}
+                                        <a href="/profile"
+                                            class="block px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden">Profile</a>
+                                        {{-- To Logout  --}}
+                                        <form method="POST" action="/logout">
+                                            @csrf
+                                            <button type="submit"
+                                                class="block w-full text-start px-4 py-2 text-sm text-gray-300 focus:bg-white/5 focus:outline-hidden cursor-pointer">Sign
+                                                out
+                                            </button>
+                                        </form>
                                     </el-menu>
                                 </el-dropdown>
                             </div>
                         </div>
                         <div class="-mr-2 flex md:hidden">
                             <!-- Mobile menu button -->
+
                             <button type="button" command="--toggle" commandfor="mobile-menu"
-                                class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
+                                class="relative inline-flex items-center justify-center rounded-md p-2 text-black-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
                                 <span class="absolute -inset-0.5"></span>
                                 <span class="sr-only">Open main menu</span>
                                 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"
@@ -81,27 +111,28 @@
                     <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
                         <!-- Current: "bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
 
-                        <x-nav-link class="block" href="/" :current="request()->is('/')">Home</x-nav-link>
+                        <x-my-nav-link class="block" href="/" :current="request()->is('/')">Home</x-my-nav-link>
 
-                        <x-nav-link class="block" href="/post" :current="request()->is('post')">Blog</x-nav-link>
+                        <x-my-nav-link class="block" href="/post" :current="request()->is('post')">Blog</x-my-nav-link>
 
-                        <x-nav-link class="block" href="/about" :current="request()->is('about')">about</x-nav-link>
+                        <x-my-nav-link class="block" href="/about" :current="request()->is('about')">about</x-my-nav-link>
 
-                        <x-nav-link class="block" href="/contact" :current="request()->is('contact')">contact</x-nav-link>
+                        <x-my-nav-link class="block" href="/contact" :current="request()->is('contact')">contact</x-my-nav-link>
 
                     </div>
                     <div class="border-t border-white/10 pt-4 pb-3">
-                        <div class="flex items-center px-5">
-                            <div class="shrink-0">
-                                <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                    alt=""
-                                    class="size-10 rounded-full outline -outline-offset-1 outline-white/10" />
-                            </div>
-                            <div class="ml-3">
-                                <div class="text-base/5 font-medium text-white">Tom Cook</div>
-                                <div class="text-sm font-medium text-gray-400">tom@example.com</div>
-                            </div>
-                            <button type="button"
+                        @auth
+                            <div class="flex items-center px-5">
+                                <div class="shrink-0">
+                                    <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                        alt="{{ Auth::user()->name }}"
+                                        class="size-10 rounded-full outline -outline-offset-1 outline-white/10" />
+                                </div>
+                                <div class="ml-3">
+                                    <div class="text-base/5 font-medium text-white">{{ Auth::user()->name }}</div>
+                                    <div class="text-sm font-medium text-gray-700">{{ Auth::user()->email }}</div>
+                                </div>
+                                {{-- <button type="button"
                                 class="relative ml-auto shrink-0 rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
                                 <span class="absolute -inset-1.5"></span>
                                 <span class="sr-only">View notifications</span>
@@ -111,18 +142,33 @@
                                         d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
                                         stroke-linecap="round" stroke-linejoin="round" />
                                 </svg>
-                            </button>
-                        </div>
-                        <div class="mt-3 space-y-1 px-2">
-                            <a href="#"
-                                class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Your
-                                profile</a>
-                            <a href="#"
-                                class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Settings</a>
-                            <a href="#"
-                                class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-white/5 hover:text-white">Sign
-                                out</a>
-                        </div>
+                            </button> --}}
+                            </div>
+                            <div class="mt-3 space-y-1 px-2">
+                                {{-- To Dasboard --}}
+                                <a href="/dashboard"
+                                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-white/5 hover:text-white cursor-pointer">Dashboard</a>
+                                {{-- To Profile  --}}
+                                <a href="#"
+                                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-white/5 hover:text-white cursor-pointer">Profile</a>
+                                {{-- To Logout  --}}
+                                <form method="POST" action="/logout">
+                                    @csrf
+                                    <button type="submit"
+                                        class="block w-full text-start rounded-md px-3 py-2 text-base font-medium text-gray-700 hover:bg-white/5 hover:text-white cursor-pointer">Sign
+                                        out
+                                    </button>
+                                </form>
+                            </div>
+                        @endauth
+
+                        {{-- jika user belum login --}}
+                        @guest
+                            <div class="my-3 space-y-1 px-2">
+                                <a href="/login" class="block text-white text-sm font-medium py-2">login</a>
+                                <a href="/login" class="block text-white text-sm font-medium py-2">register</a>
+                            </div>
+                        @endguest
                     </div>
                 </el-disclosure>
             </nav>
